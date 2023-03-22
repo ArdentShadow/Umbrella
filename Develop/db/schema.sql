83ceee46b45594479c0ffd4b4866456600dec4c0
Ryan Lierman <rylierman@gmail.com>
2023-03-22 12:51:10 -0700

    About half way through, there is a good majority done, I am unsure of how stable/functioning the schema is as I think I may be missing something.

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,33 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+CREATE DATABASE Category (
+    id int not null auto_increment,
+    category_name       VARCHAR (30) NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+CREATE DATABASE Product (
+    id int not null auto_increment,
+    product_name        VARCHAR (30) NOT NULL,
+    price               DEC NOT NULL,
+    IS_DECIMAL (super_expression), --CHECK TO MAKE SURE THIS IS WRITTEN CORRECTLY
+    stock               INT NOT NULL,
+    category_id         INT, -- check this section to make sure it is written correctly
+    PRIMARY KEY (ID)
+);
+
+CREATE DATABASE Tag (
+    id int not null auto_increment,
+    tag_name           , --need to set as a string idk how
+    PRIMARY KEY (ID)
+)
+
+CREATE DATABASE ProductTag (
+    id int not null auto_increment,
+    product_id          int,
+    -- ADD References the product model's id
+    Tag_id              int,
+    -- ADD References the tag model's id
+    PRIMARY KEY (ID)
+)
